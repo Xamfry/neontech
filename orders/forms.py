0174b57300703b1ccb2b02819949f2c117dde6cb
Xamfry <Vlad.Kopylov2573@mail.ru>
2025-04-12 02:23:37 +0300

    fix checkout

--- orders/forms.py
+++ orders/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Order
 
 class OrderCreateForm(forms.ModelForm):
-    class Mets:
+    class Meta:
         model = Order
         fields = ['user', 'first_name', 'last_name',
                     'email', 'city', 'address',
