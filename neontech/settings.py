668db6427249aa4dfc872b37ba196da1fa20c00b
Xamfry <Vlad.Kopylov2573@mail.ru>
2025-04-17 01:18:04 +0300

    modified:   neontech/settings.py

--- neontech/settings.py
+++ neontech/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3(qw(=c^elqiw_1g7e%q*+-$4^)4i8qh%rulw4j*lb^+v2fucr'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
